b471aeda6899bd0aacebcea7947faf16723302f1
TGuillerme <guillert@tcd.ie>
2018-09-06 22:33:34 +1000

    renamed landvR-package.R file

---
+++ R/landvR-package.R
@@ -0,0 +1,15 @@
+#' Utility package for studying landmark variation
+#' 
+#' This package contains utility functions to study landmark variations in a Procrustes space
+#' 
+#' @name landvR-package
+#'
+#' @docType package
+#'
+#' @author Thomas Guillerme <guillert@@tcd.ie>
+#'
+#' @keywords disparity, geometric morphometric, landmark, permutation test, random test
+#'
+#' @import geomorph
+
+NULL
