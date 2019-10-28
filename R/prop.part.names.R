0e0869690ae98c98f00a063b1641f689de114567
TGuillerme <guillert@tcd.ie>
2019-10-28 17:45:04 +1000

    prop.part.names

---
+++ R/prop.part.names.R
@@ -0,0 +1,44 @@
+#' @title Get the partitions tip labels from a tree
+#'
+#' @description Get the names of the tip labels in each bipartition (clade) of a tree.
+#'
+#' @param phy an object of class \code{"phylo"}
+#' @param singletons logical, whether to include tips as singletons (\code{TRUE}) or not (\code{FALSE} - default)
+#' 
+#' @examples
+#' ## The partition's tip labels
+#' prop.part.names(rtree(5))
+#' 
+#' ## The partition's tip labels (including singletons)
+#' prop.part.names(rtree(3), singletons = TRUE)
+#' 
+#' ## The partition's tip labels of a list of trees
+#' lapply(rmtree(2, 3), prop.part.names)
+#'
+#' @seealso \code{\link[ape]}{prop.part}
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+prop.part.names <- function(phy, singletons = FALSE) {
+    ## Sanitizing
+    if(class(phy) != "phylo") {
+        stop("phy must be a \"phylo\" object.")
+    }
+    if(class(singletons) != "logical") {
+        stop("singletons must be logical.")
+    }
+
+    ## Get the bipartitions
+    clades <- prop.part(phy)
+
+    ## Get the tips names for each clades
+    clades <- lapply(clades, function(clade, labels) labels[clade], labels = attr(clades, "labels"))
+
+    ## Add the tip names
+    if(singletons) {
+        clades <- c(clades, as.list(phy$tip.label))
+    }
+
+    return(clades)
+}
---
+++ tests/testthat/test-prop.part.names.R
@@ -0,0 +1,44 @@
+context("prop.part.names")
+
+## Test
+test_that("prop.part.names works", {
+
+    ## Sanitizing
+    error <- capture_error(prop.part.names("1"))
+    expect_equal(error[[1]], "phy must be a \"phylo\" object.")
+    error <- capture_error(prop.part.names(rtree(4), singletons = "FALSE"))
+    expect_equal(error[[1]], "singletons must be logical.")
+
+    ## Right (simple) output
+    set.seed(1)
+    test_tree <- rtree(5)
+    test_names <- prop.part.names(test_tree)
+    expect_names <- list(c("t2", "t1", "t3", "t4", "t5"), c("t1", "t3", "t4", "t5"), c("t1", "t3", "t4"), c("t3", "t4"))
+    expect_is(
+        test_names
+        , "list")
+    expect_equal(
+        length(test_names)
+        , length(ape::prop.part(test_tree)))
+    for(t in seq_along(test_names)) {
+        expect_equal(test_names[t], expect_names[t])
+    }
+
+    ## Output with tip labels
+    test_names_tips <- prop.part.names(test_tree, singletons = TRUE)
+    expect_names_tip <- c(list(c("t2", "t1", "t3", "t4", "t5"), c("t1", "t3", "t4", "t5"), c("t1", "t3", "t4"), c("t3", "t4")), as.list(test_tree$tip.label))
+    expect_is(
+        test_names_tips
+        , "list")
+    expect_equal(
+        length(test_names_tips)
+        , length(ape::prop.part(test_tree)) + Ntip(test_tree))
+    for(t in seq_along(test_names_tips)) {
+        expect_equal(test_names_tips[t], expect_names_tip[t])
+    }
+
+
+    ## Works with multifurcations
+    star <- stree(5)
+    expect_equal(prop.part.names(star), list(attr(prop.part(star), "labels")))
+})
